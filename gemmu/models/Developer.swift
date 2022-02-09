0755794ad72cf43d0651bf44912158489001fa12
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-09 18:09:53 +0700

    add developer

--- gemmu/models/Developer.swift
+++ gemmu/models/Developer.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+struct Developer: Codable {
+    let id: Int
+    let name, slug: String
+    let gamesCount: Int
+    let imageBackground: String
+    let domain: String?
+    let language: Language?
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, slug
+        case gamesCount = "games_count"
+        case imageBackground = "image_background"
+        case domain, language
+    }
+}
