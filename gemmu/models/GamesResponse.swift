c7403366bf246f902524d1dd5cd73673eabca98b
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-09 18:10:20 +0700

    add game response

--- gemmu/models/GamesResponse.swift
+++ gemmu/models/GamesResponse.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import Alamofire
+
+// MARK: - GamesResponse
+struct GamesResponse: Codable {
+    let count: Int
+    let next: String
+    let results: [GameItem]
+    let seoTitle, seoDescription, seoKeywords, seoH1: String
+    let noindex, nofollow: Bool
+    let gamesResponseDescription: String
+    let nofollowCollections: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case count, next, results
+        case seoTitle = "seo_title"
+        case seoDescription = "seo_description"
+        case seoKeywords = "seo_keywords"
+        case seoH1 = "seo_h1"
+        case noindex, nofollow
+        case gamesResponseDescription = "description"
+        case nofollowCollections = "nofollow_collections"
+    }
+}
+
