f77a146e85b45ed8b4554e58d6661846c763b777
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-09 13:52:45 +0700

    Initial Commit

---
+++ gemmu/gemmuApp.swift
@@ -0,0 +1,17 @@
+//
+//  gemmuApp.swift
+//  gemmu
+//
+//  Created by Akashaka on 09/02/22.
+//
+
+import SwiftUI
+
+@main
+struct gemmuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
