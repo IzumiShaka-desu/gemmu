f87a47b25861139a55a5a067cca281a48022981a
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-09 18:10:51 +0700

    add platform spesific

--- gemmu/models/PlatformSpesific.swift
+++ gemmu/models/PlatformSpesific.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+struct PlatformSpesific: Codable {
+    let id: Int
+    let name, slug: String
+    let yearStart: Int?
+    let gamesCount: Int
+    let imageBackground: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, slug
+        case yearStart = "year_start"
+        case gamesCount = "games_count"
+        case imageBackground = "image_background"
+    }
+}
