32e3fbe4a8bae7e4d5078c4cae26710219605378
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-09 18:09:31 +0700

    add new constants

--- gemmu/Constants.swift
+++ gemmu/Constants.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+class Constants{
+    static let profileImageUrl = "https://media.discordapp.net/attachments/916984491363409930/940466191166763048/2021042706272366ec78fe14a65eb49c3476fae33ec75b.png"
+    
+    static let profileName="Sesaka Aji Nursyah Bantani"
+    static let positionName="Mobile Developer"
+}
