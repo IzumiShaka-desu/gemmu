d027b8f0532a70d7b02ed92be2aefb9584d2634a
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-09 18:10:26 +0700

    add genre

--- gemmu/models/Genre.swift
+++ gemmu/models/Genre.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+struct Genre: Codable {
+    let id: Int
+    let name, slug: String
+    let gamesCount: Int
+    let imageBackground: String
+    let domain: String?
+    let language: Language?
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, slug
+        case gamesCount = "games_count"
+        case imageBackground = "image_background"
+        case domain, language
+    }
+}
+
+enum Language: String, Codable {
+    case eng = "eng"
+}
