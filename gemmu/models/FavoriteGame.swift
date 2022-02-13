fe2aef4ce9bf54e0bb8e0b0f53dc1cc8a67dcc0f
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-13 10:05:46 +0700

    init new file

---
+++ gemmu/models/FavoriteGame.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteGame.swift
+//  Gemmu
+//
+//  Created by Akashaka on 12/02/22.
+//
+
+import Foundation
---
+++ gemmu/views/components/AppSnackbar.swift
@@ -0,0 +1,8 @@
+//
+//  AppSnackbar.swift
+//  Gemmu
+//
+//  Created by Akashaka on 13/02/22.
+//
+
+import Foundation
