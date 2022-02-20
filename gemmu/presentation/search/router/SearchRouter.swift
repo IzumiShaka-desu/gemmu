8e2859a0f41c3e28927c9a566cda5eeec5883180
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-20 11:20:34 +0700

    create search router

--- gemmu/presentation/search/router/SearchRouter.swift
+++ gemmu/presentation/search/router/SearchRouter.swift
@@ -5,4 +5,13 @@
 //  Created by Akashaka on 20/02/22.
 //
 
-import Foundation
+import SwiftUI
+
+class SearchRouter {
+
+  func makeDetailView(for id: Int) -> some View {
+    let presenter = Injectors.sharedInstance.detailGamePresenter
+    return DetailGameView(gameId: id, presenter: presenter)
+  }
+  
+}
