5bacc12e31cc25f32d9cb2beab54b4296571cddd
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-20 11:19:23 +0700

    restructur

--- gemmu/presentation/favorite/view/FavoriteView.swift
+++ gemmu/presentation/favorite/view/FavoriteView.swift
@@ -9,16 +9,16 @@ import SwiftUI
 import RealmSwift
 
 struct FavoriteView: View {
-  @ObservedResults(FavoriteGame.self) var games
-
+  @ObservedObject  var presenter: FavoritePresenter
   var body: some View {
     ZStack(alignment: .top) {
       Color.flatDarkBackground.ignoresSafeArea()
       VStack {
-        if games.isEmpty {
+        if presenter.games.isEmpty {
           Text("You don't have any favourite games")
         } else {
-          List(games, id: \.id) {item in
+          List(presenter.games, id: \.id) {item in
+            presenter.linkBuilder(for: item.id) {
               ItemList(
                 title: item.name ,
                 releaseDate: item.releaseDate,
@@ -30,17 +30,21 @@ struct FavoriteView: View {
                 width: .infinity,
                 height: 150
               )
-              .listRowBackground(Color.flatDarkBackground)
+            }
+            .listRowBackground(Color.flatDarkBackground)
           }
         }
       }
-
+      
     }.padding(0)
   }
 }
 
 struct FavoriteView_Previews: PreviewProvider {
+  init() {
+    Injectors.sharedInstance.inject()
+  }
   static var previews: some View {
-    FavoriteView()
+    FavoriteView(presenter: Injectors.sharedInstance.favoritePresenter)
   }
 }
