6f973bd57d5d67c7bca447b51c303a32e2140dba
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-20 11:19:59 +0700

    create home router

--- gemmu/presentation/home/HomeRouter.swift
+++ gemmu/presentation/home/HomeRouter.swift
@@ -5,4 +5,13 @@
 //  Created by Akashaka on 19/02/22.
 //
 
-import Foundation
+import SwiftUI
+
+class HomeRouter {
+
+  func makeDetailView(for id: Int) -> some View {
+    let presenter = Injectors.sharedInstance.detailGamePresenter
+    return DetailGameView(gameId: id, presenter: presenter)
+  }
+  
+}
