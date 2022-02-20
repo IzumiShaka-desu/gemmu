e41f2bcd066b0183138bbc72c883daf7f6824bb8
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-20 11:18:25 +0700

    create home interactor

--- gemmu/domain/usecase/HomeInteractor.swift
+++ gemmu/domain/usecase/HomeInteractor.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Combine
+
+protocol HomeUseCase {
+
+  func getGames(target: String?) -> AnyPublisher<GamesEntity, Error>
+
+}
+
+class HomeInteractor: HomeUseCase {
+
+  private let repository: GamesRepositoryProtocol
+  
+  required init(repository: GamesRepositoryProtocol) {
+    self.repository = repository
+  }
+  
+  func getGames(target: String?) -> AnyPublisher<GamesEntity, Error> {
+    return repository.getGames(target: target)
+  }
+
+}
