617a3e2cd5db122868dc13c8d9b26a813002658f
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-20 11:18:38 +0700

    create search games interactor

--- gemmu/domain/usecase/SearchGamesInteractor.swift
+++ gemmu/domain/usecase/SearchGamesInteractor.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Combine
+
+protocol SearchGamesUseCase {
+
+  func searchGames(for: String) -> AnyPublisher<GamesSearchEntity, Error>
+
+}
+
+class SearchGamesInteractor: SearchGamesUseCase {
+
+  private let repository: GamesRepositoryProtocol
+  
+  required init(repository: GamesRepositoryProtocol) {
+    self.repository = repository
+  }
+  
+  func searchGames(for query: String) -> AnyPublisher<GamesSearchEntity, Error> {
+    return repository.searchGames(for: query)
+  }
+
+}
