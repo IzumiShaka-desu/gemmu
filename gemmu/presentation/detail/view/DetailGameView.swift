843e90ca24b576bce6d8ecddbf854325ae38e370
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-20 11:16:16 +0700

    restructure

--- gemmu/presentation/detail/view/DetailGameView.swift
+++ gemmu/presentation/detail/view/DetailGameView.swift
@@ -9,152 +9,167 @@ import NetworkImage
 import PopupView
 
 struct DetailGameView: View {
-  var gameId: Int
-  @ObservedObject var controller: DetailGameViewController
-
-  init(gameId: Int, controller: DetailGameViewController) {
-    self.gameId=gameId
-    self.controller=controller
+  @ObservedObject var presenter: DetailGamePresenter
+  var idGame: Int
+  init(gameId: Int, presenter: DetailGamePresenter) {
+    self.idGame = gameId
+    self.presenter = presenter
+    
     UITabBar.appearance().backgroundColor = UIColor.flatDarkBackground
     UINavigationBar.appearance().backgroundColor = UIColor.flatWhiteBackground
     UINavigationBar.appearance().barTintColor = UIColor.white
   }
-
+  
   var body: some View {
-
+    
     ZStack {
       Color.flatDarkBackground.ignoresSafeArea()
       VStack {
-        if self.controller.isLoading {
+        if self.presenter.isLoading {
           ProgressView().padding(16)
         } else {
-
-          if let gameDetail=controller.data {
-            PageBody(gameDetail: gameDetail, controller: controller)
+          
+          if let gameDetail=presenter.gameDetail {
+            if  presenter.isError {
+              Text(presenter.errorMessage)
+                .frame(
+                  height: 50,
+                  alignment: .center
+                )
+                .padding(16)
+                .font(.headline)
+            } else {
+              PageBody(gameDetail: gameDetail, presenter: presenter)
+            }
           } else {
             Text(" data not Loaded")
           }
-
+          
         }
-
-      }.onAppear {
-        controller.fetchItem(gameId)
+        
       }
+      
+    }.onAppear {
+      presenter.loadDetail(for: idGame)
+      presenter.checkIsFavorited()
     }
-
+    
   }
-
+  
 }
 private struct PageBody: View {
-  var gameDetail: GameDetailResponse
-  @State var isShowingPopUp: Bool = false
-  @ObservedObject var controller: DetailGameViewController
+  var gameDetail: GameDetailEntity
+  @ObservedObject var presenter: DetailGamePresenter
   var body: some View {
     ZStack {
       Color.flatDarkBackground.ignoresSafeArea()
       VStack {
         HStack {
-          NetworkImage(url: URL(string: gameDetail.backgroundImage)) { image in
+          NetworkImage(url: URL(string: gameDetail.imageUrl)) { image in
             image.resizable()
-
+            
           } placeholder: {
             ProgressView()
-
+            
           } fallback: {
             Image(systemName: "photo")
-
+            
           }
           .frame(width: 150, height: 150)
           .clipped()
           .background(Color.white)
           .cornerRadius(10)
+          
           VStack(alignment: .leading) {
-
+            
             Text(gameDetail.name)
               .font(.title)
               .fixedSize(
                 horizontal: false,
                 vertical: true
               )
-
-            Text(gameDetail.publishers.first?.name ?? "-")
+            
+            Text(gameDetail.publisher)
               .font(.subheadline)
+            
             HStack {
               Image(systemName: "star.fill")
               Text(String(format: "%.1f", gameDetail.rating))
                 .font(.subheadline)
-
+              
             }
             Button(action: {
-              controller.changeStatusFavorited()
-              self.isShowingPopUp=true
+              presenter.changeStatusFavorited()
             },
                    label: {
-              if controller.isFavorited {
+              if presenter.isFavorited {
                 Image(systemName: "star.square.fill")
                 Text("Favourited")
-
+                
               } else {
                 Image(systemName: "plus.circle")
                 Text("add to favourite")
-
+                
               }
             })
           }
-
+          
         }.padding(8)
-
+        
         Text("Release date")
           .font(.title3)
-        Text(gameDetail.released)
+        Text(gameDetail.releaseDate)
         Text("Platforms")
           .font(.title3)
-        HStack {
-          ForEach(gameDetail.parentPlatforms, id: \.platform.id) { platform in
-            TagsCard(
-              text: platform.platform.name,
-              bgColor: Color.pink
-            )
+        GeometryReader {geometry in
+          ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+              ForEach(gameDetail.platforms, id: \.self) { platform in
+                TagsCard(
+                  text: platform,
+                  bgColor: Color.pink
+                )
+              }
+            }.frame(width: geometry.size.width)
           }
-
-        }
+        }.frame(height: 56)
         Text("Genre")
           .font(.title3)
         HStack {
-          ForEach(gameDetail.genres, id: \.id) { genre in
+          ForEach(gameDetail.genres, id: \.self) { genre in
             TagsCard(
-              text: genre.name,
+              text: genre,
               bgColor: Color.pink
             )
           }
-
+          
         }
         Text("Description")
           .font(.title3)
-
+        
         ScrollView {
-          Text(gameDetail.descriptionRaw)
+          Text(gameDetail.description)
             .font(.body)
             .padding(16)
         }
-
+        
       }
     }.popup(
-      isPresented: $isShowingPopUp,
+      isPresented: $presenter.isShowingPopUp,
       type: .toast,
       position: .bottom,
       animation: .easeInOut,
       autohideIn: 2,
       closeOnTap: true,
       view: {
-        Toast(isFavorited: controller.isFavorited)
+        Toast(message: presenter.popUpMessage)
       }
     )
   }
-
+  
 }
 struct Toast: View {
-  var isFavorited: Bool
+  var message: String
   var body: some View {
     ZStack {
       Color.flatDarkCardBackground
@@ -169,20 +184,23 @@ struct Toast: View {
           .foregroundColor(Color.white)
           .padding()
         Spacer()
-        Text(isFavorited ? "added to favourite" : "removed from favourite")
+        Text(message)
           .foregroundColor(.white)
         Spacer()
         Spacer()
       }
       .padding()
     }.frame(height: 50)
-    .cornerRadius(12)
-    .padding()
-
+      .cornerRadius(12)
+      .padding()
+    
   }
 }
 struct DetailGameView_Previews: PreviewProvider {
+  init() {
+    Injectors.sharedInstance.inject()
+  }
   static var previews: some View {
-    DetailGameView(gameId: 3498, controller: DetailGameViewController.instance)
+    DetailGameView(gameId: 3498, presenter: Injectors.sharedInstance.detailGamePresenter)
   }
 }
