d45c18a83f2ef86a717f2734e618c4db43ff6da9
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-20 11:19:53 +0700

    creat ehome presenter

--- gemmu/presentation/home/presenter/HomePresenter.swift
+++ gemmu/presentation/home/presenter/HomePresenter.swift
@@ -9,23 +9,26 @@ import SwiftUI
 import Combine
 
 class HomePresenter: ObservableObject {
-
+  
   private var cancellables: Set<AnyCancellable> = []
-//  private let router = HomeRouter()
+  private let router: HomeRouter
   private let homeUseCase: HomeUseCase
-
+  
   @Published var games: [GameItemEntity] = []
   @Published var errorMessage: String = ""
-  @Published var isLoading: Bool = false
+  @Published var isLoading: Bool = true
   @Published var isError: Bool = false
   var next: String?
   
-  init(homeUseCase: HomeUseCase) {
+  init(homeUseCase: HomeUseCase, router: HomeRouter) {
     self.homeUseCase = homeUseCase
+    self.router = router
   }
   
   func getGames() {
-    isLoading = true
+    if next == nil {
+      isLoading = true
+    }
     homeUseCase.getGames(target: next)
       .receive(on: RunLoop.main)
       .sink(receiveCompletion: { completion in
@@ -43,13 +46,15 @@ class HomePresenter: ObservableObject {
       })
       .store(in: &cancellables)
   }
-//
-//  func linkBuilder<Content: View>(
-//    for category: CategoryModel,
-//    @ViewBuilder content: () -> Content
-//  ) -> some View {
-//    NavigationLink(
-//      destination: router.makeDetailView(for: category)) { content() }
-//  }
-
+  
+  func linkBuilder<Content: View>(
+    for id: Int,
+    @ViewBuilder content: () -> Content
+  ) -> some View {
+    NavigationLink(
+      destination: router.makeDetailView(for: id)) { content() }
+    .padding(0)
+    .buttonStyle(PlainButtonStyle())
+  }
+  
 }
