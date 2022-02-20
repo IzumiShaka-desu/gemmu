f96c3b2f1e32329a5cd126a6b2320ca2cd839870
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-20 11:19:00 +0700

    create favorite presenter

--- gemmu/presentation/favorite/presenter/FavoritePresenter.swift
+++ gemmu/presentation/favorite/presenter/FavoritePresenter.swift
@@ -5,4 +5,25 @@
 //  Created by Akashaka on 20/02/22.
 //
 
-import Foundation
+import Combine
+import RealmSwift
+import SwiftUI
+
+class FavoritePresenter: ObservableObject {
+  @ObservedResults(FavoriteGame.self) var games
+  private let router: FavoriteRouter
+
+  init(router: FavoriteRouter) {
+    self.router = router
+  }
+  
+  func linkBuilder<Content: View>(
+    for id: Int,
+    @ViewBuilder content: () -> Content
+  ) -> some View {
+    NavigationLink(
+      destination: router.makeDetailView(for: id)) { content() }
+    .padding(0)
+    .buttonStyle(PlainButtonStyle())
+  }
+}
