5477fde45da1bad7f95812ed4142aa1778b5bbf1
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-28 17:48:45 +0700

    update injects

--- gemmu/core/DI/Injector.swift
+++ gemmu/core/DI/Injector.swift
@@ -9,25 +9,41 @@ import Foundation
 import DIContainer
 import RealmSwift
 import Network
+import Common
+import Home
+import Detail
+import Search
+import Favorite
+
 extension InjectIdentifier {
   
   // datasouce
   
-  static var remoteService: InjectIdentifier<RemoteDataSource> {
-    .by(type: RemoteDataSource.self, key: "remoteDS")
+  static var homeService: InjectIdentifier<HomeRemoteDataSourceProtocol> {
+    .by(type: HomeRemoteDataSourceProtocol.self, key: "homeDS")
   }
-  static var localService: InjectIdentifier<LocaleDataSource> {
-    .by(type: LocaleDataSource.self, key: "localDS")
-    
+  static var detailService: InjectIdentifier<DetailDataSourceProtocol> {
+    .by(type: DetailDataSourceProtocol.self, key: "detailDS")
+  }
+  static var searchService: InjectIdentifier<SearchRemoteDataSourceProtocol> {
+    .by(type: SearchRemoteDataSourceProtocol.self, key: "searchDS")
+  }
+  static var localService: InjectIdentifier<LocaleDataSourceProtocol> {
+    .by(type: LocaleDataSourceProtocol.self, key: "localDS")
   }
   
   // repository
   
-  static var gamesRepo: InjectIdentifier<GamesRepositoryProtocol> {
-    .by(type: GamesRepositoryProtocol.self, key: "gamesRepo")
-    
+  static var homeRepo: InjectIdentifier<HomeRepositoryProtocol> {
+    .by(type: HomeRepositoryProtocol.self, key: "homeRepo")
   }
-  
+  static var detailRepo: InjectIdentifier<DetailGamesRepositoryProtocol> {
+    .by(type: DetailGamesRepositoryProtocol.self, key: "detailRepo")
+  }
+  static var searchRepo: InjectIdentifier<SearchGamesRepositoryProtocol> {
+    .by(type: SearchGamesRepositoryProtocol.self, key: "searchRepo")
+  }
+
   // interactor
   
   static var homeInteractor: InjectIdentifier<HomeInteractor> {
@@ -46,25 +62,17 @@ extension InjectIdentifier {
   
   // router
   
-  static var homeRouter: InjectIdentifier<HomeRouter> {
-    .by(type: HomeRouter.self, key: "homeRouter")
-  }
-  
-  static var favoriteRouter: InjectIdentifier<FavoriteRouter> {
-    .by(type: FavoriteRouter.self, key: "favRouter")
-  }
-  
-  static var searchRouter: InjectIdentifier<SearchRouter> {
-    .by(type: SearchRouter.self, key: "searchRouter")
+  static var homeRouter: InjectIdentifier<Router> {
+    .by(type: Router.self, key: "router")
   }
   
   // presenter
   
-  static var homePresenter: InjectIdentifier<HomePresenter> {
+  static var homePresenter: InjectIdentifier<HomePresenter<DetailGameView>> {
     .by(type: HomePresenter.self, key: "homePresenter")
     
   }
-  static var searchPresenter: InjectIdentifier<SearchGamesPresenter> {
+  static var searchPresenter: InjectIdentifier<SearchGamesPresenter<DetailGameView>> {
     .by(type: SearchGamesPresenter.self, key: "seachPresenter")
   }
   
@@ -72,7 +80,7 @@ extension InjectIdentifier {
     .by(type: DetailGamePresenter.self, key: "detailGamePresenter")
   }
   
-  static var favoritePresenter: InjectIdentifier<FavoritePresenter> {
+  static var favoritePresenter: InjectIdentifier<FavoritePresenter<DetailGameView>> {
     .by(type: FavoritePresenter.self, key: "favPresenter")
   }
   
@@ -86,61 +94,36 @@ class Injectors {
   @Injected(.favoritePresenter) var favoritePresenter: FavoritePresenter
   
   func inject() {
-    
-    // datasource
-    
-    Container.standard.register(.remoteService) {_ in RemoteDataSource.sharedInstance}
-    
-    Container.standard.register(.localService) {_ in
-      let realm  = try Realm()
-      return  LocaleDataSource.sharedInstance(realm)
-    }
-    
-    // repository
-    
-    Container.standard.register(.gamesRepo) {resolver in
-      let localService = try resolver.resolve(.localService)
-      let remoteService = try resolver.resolve(.remoteService)
-      return GamesRepository.sharedInstance(localService, remoteService)
-    }
-    
-    // interactor
-    
-    Container.standard.register(.homeInteractor) {resolver in
-      let repo = try resolver.resolve(.gamesRepo)
-      return HomeInteractor.init(repository: repo)
-    }
-    
-    Container.standard.register(.searchInteractor) {resolver in
-      let repo = try resolver.resolve(.gamesRepo)
-      return SearchGamesInteractor.init(repository: repo)
-    }
-    
-    Container.standard.register(.detailGameInteractor) { resolver in
-      let repo = try resolver.resolve(.gamesRepo)
-      return DetailGameInteractor.init(repository: repo)
-    }
-    
+    injectDatasources()
+    injectRepositories()
+    injectInteractor()
+    injectRouter()
+    injectPresenter()
+  }
+  
+  func injectRouter() {
     // router
     
-    Container.standard.register(.homeRouter) {_ in HomeRouter()}
+    Container.standard.register(.homeRouter) {_ in Router.sharedInstance}
     
-    Container.standard.register(.favoriteRouter) {_ in FavoriteRouter()}
-    
-    Container.standard.register(.searchRouter) {_ in SearchRouter()}
-    
+  }
+  func injectPresenter() {
     // presenter
     
     Container.standard.register(.homePresenter) {resolver in
       let interactor = try resolver.resolve(.homeInteractor)
       let router = try resolver.resolve(.homeRouter)
-      return HomePresenter.init(homeUseCase: interactor, router: router)
+      return HomePresenter(homeUseCase: interactor) {id in
+        router.makeDetailView(for: id)
+      }
     }
     
     Container.standard.register(.searchPresenter) {resolver in
       let interactor = try resolver.resolve(.searchInteractor)
-      let router =  try resolver.resolve(.searchRouter)
-      return SearchGamesPresenter.init(searchUseCase: interactor, router: router)
+      let router =  try resolver.resolve(.homeRouter)
+      return SearchGamesPresenter.init(searchUseCase: interactor) {id in
+        router.makeDetailView(for: id)
+      }
     }
     
     Container.standard.register(.detailGamePresenter) {resolver in
@@ -148,31 +131,64 @@ class Injectors {
       return DetailGamePresenter.init(usecase: interactor)
     }
     Container.standard.register(.favoritePresenter) {resolver in
-      let router =  try resolver.resolve(.favoriteRouter)
-      return FavoritePresenter.init(router: router)
+      let router =  try resolver.resolve(.homeRouter)
+      return FavoritePresenter {id in
+        router.makeDetailView(for: id)
+      }
+    }
+  }
+     func injectInteractor() {
+      // interactor
+      
+      Container.standard.register(.homeInteractor) {resolver in
+        let repo = try resolver.resolve(.homeRepo)
+        return HomeInteractor(repository: repo)
+      }
+      
+      Container.standard.register(.searchInteractor) {resolver in
+        let repo = try resolver.resolve(.searchRepo)
+        return SearchGamesInteractor.init(repository: repo)
+      }
+      
+      Container.standard.register(.detailGameInteractor) { resolver in
+        let repo = try resolver.resolve(.detailRepo)
+        return DetailGameInteractor(repository: repo)
+      }
+    }
+     func injectDatasources() {
+      // datasource
+      
+      Container.standard.register(.homeService) {_ in HomeRemoteDataSource.sharedInstance}
+      
+      Container.standard.register(.detailService) {_ in DetailRemoteDataSource.sharedInstance}
+      
+      Container.standard.register(.searchService) {_ in SearchRemoteDataSource.sharedInstance}
+      
+      Container.standard.register(.localService) {_ in
+        let realm  = try Realm()
+        return  LocaleDataSource.sharedInstance(realm)
+      }
+      
+    }
+     func injectRepositories() {
+      // repository
+      Container.standard.register(.homeRepo) {resolver in
+        let homeService = try resolver.resolve(.homeService)
+        return HomeRepository.sharedInstance(homeService)
+      }
+      Container.standard.register(.detailRepo) {resolver in
+        let detailService = try resolver.resolve(.detailService)
+        let localService = try resolver.resolve(.localService)
+        return DetailGamesRepository.sharedInstance(
+          detailService ,
+          localService
+        )
+      }
+      Container.standard.register(.searchRepo) {resolver in
+        let searchService =  try resolver.resolve(.searchService)
+        return SearchGamesRepository.sharedInstance(searchService)
+      }
+      
     }
   
-}
-  
-  //    container.register(LocaleDataSource.self) {_ in
-  //      let realm=try? Realm()
-  //      let _ = print("injecting")
-  //      return  LocaleDataSource.sharedInstance(realm)
-  //    }
-  //    container.register(RemoteDataSource.self) {_ in
-  //      return RemoteDataSource.sharedInstance
-  //    }
-  //    container.register(GamesRepository.self) {resolver in
-  //      return GamesRepository.sharedInstance(
-  //        resolver.resolve(LocaleDataSource.self)!,
-  //        resolver.resolve(RemoteDataSource.self)!
-  //      )
-  //    }
-  //    container.register(HomeInteractor.self) {resolver in
-  //      return HomeInteractor.init(repository: resolver.resolve(GamesRepository.self)!)
-  //    }
-  //
-  //    container.register(SearchGamesInteractor.self) {resolver in
-  //      return SearchGamesInteractor.init(repository: resolver.resolve(GamesRepository.self)!)
-  //    }
 }
