1b8da9e64afb56b3443898c9a16dbb66c1091238
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-28 17:48:34 +0700

    update router system

--- gemmu/core/router/Router.swift
+++ gemmu/core/router/Router.swift
@@ -4,5 +4,12 @@
 //
 //  Created by Akashaka on 28/02/22.
 //
-
-import Foundation
+import SwiftUI
+import Detail
+final class Router {
+  static let sharedInstance = Router()
+  func makeDetailView(for id: Int) -> DetailGameView {
+    let presenter = Injectors.sharedInstance.detailGamePresenter
+    return DetailGameView(gameId: id, presenter: presenter)
+  }
+}
